2c93cd57b58ee9706fae1417a0428d367151a622
Zabrina L <zabrinatan@hotmail.com>
2019-03-30 01:01:22 +1100

    fix migration for emotions

--- db/migrate/20190329133948_create_emotions.rb
+++ db/migrate/20190329133948_create_emotions.rb
@@ -1,7 +1,10 @@
 class CreateEmotions < ActiveRecord::Migration[5.2]
   def change
     create_table :emotions do |t|
-
+      t.text :emotion_name
+      t.text :voice_clip_url
+      t.text :image
+      t.float :weight
       t.timestamps
     end
   end
